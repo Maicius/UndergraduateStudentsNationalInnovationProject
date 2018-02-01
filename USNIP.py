4b3cb7ae2f1d1b2b944bec190d360d37c8c63929
mai ciu s <maicius@outlook.com>
2018-02-01 20:39:07 +0800

    计算排名

--- USNIP.py
+++ USNIP.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 import jieba
-import numpy as np
+import pandas as pd
+
 jieba.load_userdict("userdict.txt")
 
 
 def count_frequency(data_arr):
     _keyword_dict = {}
     for word in data_arr:
-        if word not in data_arr:
+        if word not in _keyword_dict:
             _keyword_dict[word] = 1
         else:
             _keyword_dict[word] += 1
@@ -16,24 +17,41 @@ def count_frequency(data_arr):
 
 class USNIP(object):
     def __init__(self):
-        self._2015_data = []
         self._2015_keyword = []
         self._2016_keyword = []
         self._2017_keyword = []
         self.waste_words = "基于 研究 技术 方法 理论 为例 实验 影响 模拟 作用 应用 工程 探究 探索 浅析 机制 坚定 分析 调研 构建 特征 " \
-                           "设计 方案 新型 及其 系统 公司 组织 平台 用于 对策 不同 使用 调查 合作 辅助 我国 地区 的 与 -"
+                           "设计 方案 新型 及其 系统 公司 组织 平台 用于 对策 不同 使用 调查 合作 辅助 我国 地区 " \
+                           "制备 大学生 开发 合成 实现 检测 中国 高校 研制 优化 服务 有限 项目 发展 装置 问题 现状 一种 结构 模型 性能 " \
+                           "模式 有限公司 试验 比较 推广 利用 特性 因素 机理 建设"
         self._2015_keyword_dict = {}
+        self.generate_waste_words_array()
 
     def calculate_keyword(self, data):
         project_names = data.sum(axis=0).values[0]
-        self.generate_waste_words_array()
-        for item in self.waste_words:
-            project_names = project_names.replace(item, '')
-        project_names = project_names.replace(' ', '')
-        self._2015_keyword = list(jieba.cut(project_names, cut_all=False))
-        print(self._2015_keyword)
-        self._2015_keyword_dict = count_frequency(self._2015_keyword)
-        print(sorted(self._2015_keyword_dict, key=lambda x:x[1], reverse=True))
+
+        _keyword = list(jieba.cut(project_names, cut_all=False))
+        _keyword = filter(self.remove_waste, _keyword)
+        _keyword_dict = count_frequency(_keyword)
+        _keyword_tuple = sorted(_keyword_dict.items(), key=lambda x: x[1], reverse=True)
+        print(_keyword_tuple)
+        return _keyword_tuple
 
     def generate_waste_words_array(self):
-        self.waste_words = self.waste_words.split(' ')
+        self.waste_words = set(self.waste_words.split(' '))
+
+    def remove_waste(self, word):
+        if word in self.waste_words or len(word) < 2:
+            return ''
+        return word
+
+    def reset_index_name(self):
+        self._2015_keyword.columns = ['rank', 'word', 'val']
+        self._2016_keyword.columns = ['rank', 'word', 'val']
+        self._2017_keyword.columns = ['rank', 'word', 'val']
+
+    def calculate_rank_diff(self):
+        all_unique_words = pd.concat(
+            [self._2015_keyword['word'], self._2016_keyword['word'], self._2017_keyword['word']],
+            ignore_index=True).drop_duplicates()
+        print(all_unique_words)
--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import pandas as pd
 from USNIP import USNIP
 if __name__ == '__main__':
     usnip = USNIP()
+    # 获取数据
     _2015_data = pd.read_excel('2015.xls', header=None)
-    usnip._2015_data = _2015_data.iloc[3:]
-    usnip.calculate_keyword(_2015_data.iloc[3:, 3:4])
+    usnip._2015_keyword = pd.DataFrame(usnip.calculate_keyword(_2015_data.iloc[3:, 3:4])).reset_index()
+
+    _2016_data = pd.read_excel('2016.xlsx', header=None, sheet_name='信息表')
+    usnip._2016_keyword = pd.DataFrame(usnip.calculate_keyword(_2016_data.iloc[3:, 5:6])).reset_index()
+
+    _2017_data = pd.read_excel('2017.xlsx', header=None)
+    usnip._2017_keyword = pd.DataFrame(usnip.calculate_keyword(_2017_data.iloc[3:, 3:4])).reset_index()
+
+    # 重置列名
+    usnip.reset_index_name()
+    usnip.calculate_rank_diff()
+
     pass
